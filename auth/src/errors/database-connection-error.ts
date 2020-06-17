fb1bed1d1d53deeb6592cd132d31bdc8a2453fba
vitcool@ukr.net <vitcool@ukr.net>
2020-06-17 19:23:25 +0300

    Init commit (error handling is added)

---
+++ auth/src/errors/database-connection-error.ts
@@ -0,0 +1,16 @@
+import { CustomError } from './customError';
+
+export class DatabaseConnectionError extends CustomError {
+  statusCode = 500;
+  reason = 'Error connecting to the DB';
+
+  constructor() {
+    super('Error connecting to DB');
+
+    Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+  }
+
+  serializeErrors() {
+    return [{ message: this.reason }]
+  }
+}
