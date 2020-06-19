3e40473e65d3345a9dbdde881cb99bc2f355a29e
vitcool@ukr.net <vitcool@ukr.net>
2020-06-19 11:34:46 +0300

    Validation request middleware is added + prettierrc

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,22 +1,24 @@
-import express,  { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
+import express, { Request, Response } from "express";
+import { body } from "express-validator";
 
-import { RequestValidationError } from '../errors/request-validation-error';
+import { validateRequest } from "../middlewares/validate-request";
+
 
 const router = express.Router();
 
-router.post('/api/users/signin', [
-    body('email').isEmail().withMessage('Email must be valid'),
-    body('password').trim().notEmpty().withMessage('You must provide a password')
+router.post(
+  "/api/users/signin",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .notEmpty()
+      .withMessage("You must provide a password"),
   ],
+  validateRequest,
   (req: Request, res: Response) => {
-    const errors = validationResult(req);
-  
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
-    }
-    res.send('hi there!');
-  }
+    res.send("hi there!");
+  },
 );
 
 export { router as signinRouter };
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,49 +1,51 @@
-import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
-import jwt from 'jsonwebtoken';
-import { User } from '../models/user';
+import express, { Request, Response } from "express";
+import { body } from "express-validator";
+import jwt from "jsonwebtoken";
+import { User } from "../models/user";
+import { validateRequest } from "../middlewares/validate-request";
 
-import { RequestValidationError } from '../errors/request-validation-error';
-import { BadRequestError } from '../errors/bad-request-error';
-
+import { BadRequestError } from "../errors/bad-request-error";
 
 const router = express.Router();
 
-router.post('/api/users/signup', [
-  body('email').isEmail().withMessage('Email must be valid'),
-  body('password').trim().isLength({ min: 4, max: 20}).withMessage('Password must be between 4 and 20 characters')
-], async (req: Request, res: Response) => {
-  const errors = validationResult(req);
-  
-  if (!errors.isEmpty()) {
-    throw new RequestValidationError(errors.array());
-  }
-  const { email, password } = req.body;
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must be between 4 and 20 characters"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
 
-  const existingUser = await User.findOne({ email });
+    const existingUser = await User.findOne({ email });
 
-  if (existingUser) {
-    throw new BadRequestError('Email in use');
-  }
+    if (existingUser) {
+      throw new BadRequestError("Email in use");
+    }
 
-  const user = User.build({ email, password });
-  await user.save();
+    const user = User.build({ email, password });
+    await user.save();
 
-  //generate jwt 
-  const userJwt = jwt.sign(
-    {
-      id: user.id,
-      email: user.email
-    },
-    process.env.JWT_KEY!
-  );
+    //generate jwt
+    const userJwt = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      process.env.JWT_KEY!,
+    );
 
-  // store it insode the session object
-  req.session = {
-    jwt: userJwt
-  };
+    // store it insode the session object
+    req.session = {
+      jwt: userJwt,
+    };
 
-  res.status(201).send(user);
-});
+    res.status(201).send(user);
+  },
+);
 
 export { router as signupRouter };
